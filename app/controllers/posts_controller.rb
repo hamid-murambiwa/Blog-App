bf251c6af4ee4eea001fb25be5845ce1e32ee253
hamid-murambiwa <machipisajunior@gmail.com>
2022-02-16 10:30:54 +0200

    Fix linters errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
+# Posts controller
 class PostsController < ApplicationController
-    def index
-    end
-    def show
-    end
-  end
+  def index; end
+
+  def show; end
+end
