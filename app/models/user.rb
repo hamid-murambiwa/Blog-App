08234b84876383ff10cced40a5a16e2b856319ff
hamid-murambiwa <machipisajunior@gmail.com>
2022-02-22 05:39:43 +0200

    Add recent_posts method to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_many :posts, foreign_key: :user_id
     has_many :commeents, foreign_key: :user_id
     has_many :likes, foreign_key: :user_id
+
+    def recent_posts
+        Post.last(3)
+    end
 end
