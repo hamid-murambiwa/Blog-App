a289ad88bc0d461d5afe94828d6c310bdac03030
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-08 16:04:15 +0200

    Fix syntax errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :posts_counter, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :photo, :bio,  :email, :password, :current_password)
+      u.permit(:name, :photo, :bio, :email, :password, :current_password)
     end
   end
 end
