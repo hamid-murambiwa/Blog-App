2f9ffe6fd404ec88e8802b7897cc6b4aedb729ce
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-03-11 16:21:08 +0100

    Add create method to comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,6 +5,21 @@ class Api::V1::CommentsController < ApplicationController
         render json: @comments, status: :ok
       end
 
-    def create
-    end
+      def create
+        @comment = Comment.new(comment_params)
+        @comment.post_id = params[:post_id]
+        @comment.user = current_user
+    
+        if @comment.save
+          render json: { status: 201, message: 'Comment created successfully!', content: { comment: @comment } }
+        else
+          render json: @comment.errors, status: :bad_request, message: 'Failed!, try again'
+        end
+      end
+    
+      private
+    
+      def comment_params
+        params.require(:comment).permit(:text)
+      end
 end
