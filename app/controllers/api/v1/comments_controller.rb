5c761c33fbb7bbb2863b1bbc49727007e3119034
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-11 17:03:09 +0200

    Add API endpoint to list all comments for a user's post

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::CommentsController < ApplicationController
+    def index
+        @post = Post.find(params[:post_id])
+        @comments = @post.comments
+        render json: @comments, status: :ok
+      end
+
+    def create
+    end
+end
